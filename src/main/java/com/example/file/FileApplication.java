477abd64f831e8decfaf0bbeb28d06d1d46e0d5b
pihu2810 <vishaka.kadam01@gmail.com>
2022-06-30 17:17:54 +0530

    Add to file Upload

---
+++ src/main/java/com/example/file/FileApplication.java
@@ -0,0 +1,20 @@
+package com.example.file;
+
+import com.example.file.model.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class FileApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FileApplication.class, args);
+	}
+
+}
